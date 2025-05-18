ff142629f9fdad26de9e4bbc58cfbcff13a96afe
Mary11994 <declansweet21@gmail.com>
2025-05-18 17:00:13 +0300

    fix: updated Vite base path for Vercel deployment

--- contact-form/vite.config.js
+++ contact-form/vite.config.js
@@ -10,5 +10,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ Fixes path issues on Vercel or subdirectories
+  base: '/', // ✅ Fixes path issues on Vercel or subdirectories
 })
